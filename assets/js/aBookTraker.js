8f698373fe9cd7c0ea5e4543feb0dd41d8795c29
paikwiki <paikwiki@gmail.com>
2018-03-12 10:26:02 +0900

    Add event tracker for ttb

---
+++ assets/js/aBookTraker.js
@@ -0,0 +1,23 @@
+ga('create', 'UA-115101412-1', 'auto');
+
+function clickABookLink(event) {
+  ga('send', 'event', {
+    eventCategory: 'Aladin TTB2',
+    eventAction: 'click',
+    eventLabel: document.title
+  });
+}
+
+function addABookTracker(link) {
+  var url = link.getAttribute('href');
+  if ( url.search('ttbkey') ) {
+    link.addEventListener('click', function(e) {
+      clickABookLink(e);
+    });
+  }
+}
+
+var links = document.querySelectorAll('a');
+links.forEach(function(i) {
+  addABookTracker(i);
+});
